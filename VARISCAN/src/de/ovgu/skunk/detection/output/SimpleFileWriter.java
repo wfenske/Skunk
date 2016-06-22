88293b5c21b0f48cb79ea6439a537e01c7058525
wfenske <wcomm@gmx.de>
2016-06-22 12:22:56 +0200

    Changed package structure

---
+++ VARISCAN/src/de/ovgu/skunk/detection/output/SimpleFileWriter.java
@@ -0,0 +1,39 @@
+package de.ovgu.skunk.detection.output;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.apache.commons.io.FileUtils;
+
+public class SimpleFileWriter {
+    private SortedSet<String> fileNames = new TreeSet<>();
+    private String dir = null;
+
+    public void write(File f, String contents) throws IOException {
+        FileUtils.write(f, contents);
+        if (dir == null)
+            dir = f.getCanonicalFile().getParent();
+        fileNames.add(f.getName());
+    }
+
+    public String prettyFileNameList() {
+        StringBuilder b = new StringBuilder();
+        for (String n : fileNames) {
+            if (b.length() > 0) {
+                b.append(", ");
+            }
+            b.append(n);
+        }
+        return b.toString();
+    }
+
+    /**
+     * @return Name of the directory in which the first file handed to
+     *         {@link #write(File, String)} was located.
+     */
+    public String getDir() {
+        return dir;
+    }
+}
---
+++ VARISCAN/src/de/ovgu/skunk/detection/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Everything that is related to smell detection within Skunk.
+ */
+/**
+ * @author wfenske
+ *
+ */
+package de.ovgu.skunk.detection;
