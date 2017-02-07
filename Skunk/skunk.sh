0ff638381e146be2e32c3aa21ed3cf469e381a2b
wfenske <wcomm@gmx.de>
2017-02-07 16:10:17 +0100

    Fixed skunk scrips

--- Skunk/skunk.sh
+++ Skunk/skunk.sh
@@ -1,47 +1,45 @@
 #!/usr/bin/env sh
 
-SKUNK_CP=""
+CP=""
 
 add_to_cp()
 {
     if [ -n "$1" ]
     then
-	SKUNK_CP="$SKUNK_CP${SKUNK_CP:+:}${1}"
+        if [ ! -e "$1" ]
+        then
+            echo "Warn: file not found: \`$1'" >&2
+        fi
+        CP="$CP${CP:+:}${1}"
     fi
 }
 
-skunk_dir=$(dirname "$0")/../..
-
-LOG4J_PATH="${skunk_dir}"/metricminer2/lib/apache-log4j-2.6-bin
-
-# commons-csv
-add_to_cp "${skunk_dir}"/Skunk/VARISCAN/lib/commons-csv-1.1.jar
-
-# commons-io
-add_to_cp "${skunk_dir}"/Skunk/VARISCAN/lib/commons-io-2.4.jar
-
-# xmlpull
-add_to_cp "${skunk_dir}"/Skunk/VARISCAN/lib/xmlpull-1.1.3.1.jar
-
-# xpp3_min
-add_to_cp "${skunk_dir}"/Skunk/VARISCAN/lib/xpp3_min-1.1.4c.jar
-
-# xstream
-add_to_cp "${skunk_dir}"/Skunk/VARISCAN/lib/xstream-1.4.8.jar
+skunk_dir=$(dirname "$0")
+maven_repo=$HOME/.m2/repository
 
 # commons-cli
-add_to_cp "${skunk_dir}"/Skunk/VARISCAN/lib/commons-cli-1.3.1/commons-cli-1.3.1.jar
+add_to_cp "${maven_repo}"/commons-cli/commons-cli/1.3/commons-cli-1.3.jar
 
-# log4j-api
-add_to_cp $LOG4J_PATH/log4j-api-2.6.jar
+# commons-csv
+add_to_cp "${maven_repo}"/org/apache/commons/commons-csv/1.1/commons-csv-1.1.jar
 
-# log4j-core
-add_to_cp $LOG4J_PATH/log4j-core-2.6.jar
+# commons-io
+add_to_cp "${maven_repo}"/commons-io/commons-io/2.4/commons-io-2.4.jar
 
-# log4j-1.2-api
-add_to_cp $LOG4J_PATH/log4j-1.2-api-2.6.jar
+# xmlpull
+add_to_cp "${maven_repo}"/xmlpull/xmlpull/1.1.3.1/xmlpull-1.1.3.1.jar
 
-# Skunk classes
-add_to_cp "${skunk_dir}"/Skunk/VARISCAN/bin
+# xpp3_min
+add_to_cp "${maven_repo}"/xpp3/xpp3_min/1.1.4c/xpp3_min-1.1.4c.jar
 
-exec java -cp $SKUNK_CP de.ovgu.skunk.detection.main.Skunk "$@"
+# xstream
+add_to_cp "${maven_repo}"/com/thoughtworks/xstream/xstream/1.4.9/xstream-1.4.9.jar
+
+# Log4j
+add_to_cp "${maven_repo}"/log4j/log4j/1.2.14/log4j-1.2.14.jar
+add_to_cp "${maven_repo}"/org/apache/logging/log4j/log4j-api/2.1/log4j-api-2.1.jar
+
+# Skunk class files
+add_to_cp "${skunk_dir}"/target/classes
+
+exec java -cp "$CP" de.ovgu.skunk.detection.main.Skunk "$@"
