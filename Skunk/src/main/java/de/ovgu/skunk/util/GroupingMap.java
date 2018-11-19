0c7947676759837e0c26ba398c3668ea8e14d33b
wfenske <wcomm@gmx.de>
2018-11-19 14:27:08 +0100

    New method remove for GroupingMap

--- Skunk/src/main/java/de/ovgu/skunk/util/GroupingMap.java
+++ Skunk/src/main/java/de/ovgu/skunk/util/GroupingMap.java
@@ -38,6 +38,37 @@ public abstract class GroupingMap<K, V, C extends Collection<V>> {
         return valuesForKey;
     }
 
+    /**
+     * Remove the given mapping from the map.  If no other values are associated with the same key, also remove the
+     * collection for holding the values.
+     *
+     * @param key
+     * @param value
+     * @return <code>true</code> if a mapping from key to value existed prior to calling this method
+     */
+    public boolean remove(K key, V value) {
+        C valuesForKey = get(key);
+        if (valuesForKey == null) {
+            return false;
+        }
+        boolean removedValue = valuesForKey.remove(value);
+        if (valuesForKey.isEmpty()) {
+            remove(key);
+        }
+        return removedValue;
+    }
+
+    /**
+     * Remove all mappings for the given key from the map.  Return the current collection of values for this
+     * key, or <code>null</code>, if the key was unmapped prior to calling this method.
+     *
+     * @param key The key whose mapping to remove
+     * @return The collection of values for this key or <code>null</code> if the key was not mapped.
+     */
+    public C remove(K key) {
+        return map.remove(key);
+    }
+
     public C get(K key) {
         return map.get(key);
     }
