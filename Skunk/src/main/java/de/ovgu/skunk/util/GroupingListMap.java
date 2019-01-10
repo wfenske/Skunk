0bcb517651bf3094c3dfb859a1f1d28f6e3de9f3
wfenske <wcomm@gmx.de>
2019-01-10 15:59:51 +0100

    Trying to lower memory footprint

--- Skunk/src/main/java/de/ovgu/skunk/util/GroupingListMap.java
+++ Skunk/src/main/java/de/ovgu/skunk/util/GroupingListMap.java
@@ -4,9 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupingListMap<K, V> extends GroupingMap<K, V, List<V>> {
+    private final int defaultListCapacity;
+
+    public GroupingListMap() {
+        this.defaultListCapacity = -1;
+    }
+
+    public GroupingListMap(int defaultListCapacity) {
+        this.defaultListCapacity = defaultListCapacity;
+    }
 
     @Override
     protected List<V> newCollection() {
-        return new ArrayList<>();
+        if (defaultListCapacity > 0) {
+            return new ArrayList<>(defaultListCapacity);
+        } else {
+            return new ArrayList<>();
+        }
     }
 }
