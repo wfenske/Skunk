11ec5c3e690c45b9804fbab4a3504cdb2742f62f
Wolfram Fenske <wfenske@ovgu.de>
2019-01-13 15:28:42 +0100

    Added new method emptyMap

--- Skunk/src/main/java/de/ovgu/skunk/util/GroupingListMap.java
+++ Skunk/src/main/java/de/ovgu/skunk/util/GroupingListMap.java
@@ -1,7 +1,9 @@
 package de.ovgu.skunk.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class GroupingListMap<K, V> extends GroupingMap<K, V, List<V>> {
     private final int defaultListCapacity;
@@ -12,6 +14,24 @@ public class GroupingListMap<K, V> extends GroupingMap<K, V, List<V>> {
 
     public GroupingListMap(int defaultListCapacity) {
         this.defaultListCapacity = defaultListCapacity;
+    }
+
+    private static class EmptyGroupingListMap<K, V> extends GroupingListMap<K, V> {
+        @Override
+        protected Map<K, List<V>> newMap() {
+            return Collections.emptyMap();
+        }
+
+        @Override
+        protected List<V> newCollection() {
+            return Collections.emptyList();
+        }
+    }
+
+    private static final EmptyGroupingListMap EMPTY_GROUPING_LIST_MAP = new EmptyGroupingListMap();
+
+    public static <K, V> GroupingListMap<K, V> emptyMap() {
+        return EMPTY_GROUPING_LIST_MAP;
     }
 
     @Override
